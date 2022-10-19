22039fc8591c4e84d89b2ee8cbf1a5df550f9408
Kankit40 <Kankit40@gmail.com>
2022-10-19 12:46:52 +0530

    Added: UC4 Play till win

--- src/com/bridgelabz/SnakeAndLadder.java
+++ src/com/bridgelabz/SnakeAndLadder.java
@@ -4,43 +4,53 @@ public class SnakeAndLadder {
     public static void main(String[] args) {
 
         int playerPosition = 0;
+        int roll = 0;
 
-        int dice = (int)(Math.random() * 6);
+        while(playerPosition<100){
+            roll++;
 
-        if(dice ==1) {
-            System.out.println("The output is One");
-        }else if(dice ==2) {
-            System.out.println("The output is Two");
-        }else if(dice ==3) {
-            System.out.println("The output is Three");
-        }else if(dice ==4) {
-            System.out.println("The output is Four");
-        }else if(dice ==5) {
-            System.out.println("The output is Five");
-        }else {
-            System.out.println("The output is Six");
-        }
+            int dice = (int)(Math.random() * 6);
 
-        System.out.println("-------------------------------------");
-
-        int checkPlay = (int)(Math.random() * 3);
-
-        if(checkPlay ==1) {
-            System.out.println("The player gets No Play");
-            playerPosition += 0;
-            System.out.println("The player moves to " + playerPosition);
-        }else if(checkPlay ==2) {
-            System.out.println("The player gets Ladder");
-            playerPosition += dice;
-            System.out.println("The player moves to " + playerPosition);
-        }else {
-            System.out.println("The player gets Snake");
-            if(playerPosition<dice) {
-                playerPosition += 0;
-                System.out.println("The player stays at " + playerPosition);
+            if(dice ==1) {
+                System.out.println("The output is One");
+            }else if(dice ==2) {
+                System.out.println("The output is Two");
+            }else if(dice ==3) {
+                System.out.println("The output is Three");
+            }else if(dice ==4) {
+                System.out.println("The output is Four");
+            }else if(dice ==5) {
+                System.out.println("The output is Five");
             }else {
-                playerPosition += dice;
+                System.out.println("The output is Six");
+            }
+
+            System.out.println("-------------------------------------");
+
+            int checkPlay = (int)(Math.random() * 3);
+
+            if(checkPlay ==1) {
+                System.out.println("The player gets No Play");
+                playerPosition += 0;
                 System.out.println("The player moves to " + playerPosition);
+            }else if(checkPlay ==2) {
+                System.out.println("The player gets Ladder");
+                if(dice+playerPosition>100){
+                    playerPosition = dice;
+                    System.out.println("The player moves to " + playerPosition);
+                }else {
+                    playerPosition += dice;
+                    System.out.println("The player moves to " + playerPosition);
+                }
+            }else {
+                System.out.println("The player gets Snake");
+                if(playerPosition<dice) {
+                    playerPosition = 0;
+                    System.out.println("The player stays at " + playerPosition);
+                }else {
+                    playerPosition -= dice;
+                    System.out.println("The player moves to " + playerPosition);
+                }
             }
         }
     }
